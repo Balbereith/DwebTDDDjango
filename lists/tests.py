7e128f1d7ee56c41d5cbd058812807f7eb53811f
Clement Dechy <clement.dechy@gmail.com>
2016-02-24 09:54:40 +0100

    functional test refractor

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
+
 from lists.views import home_page
 
 
@@ -13,5 +14,10 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string("home.html")
+        expected_html = render_to_string("home.html",
+                                         {'new_item_text': 'A new list item'},
+                                         request=request)
+        print(expected_html)
+        print("--------------------")
+        print(response.content.decode())
         self.assertEqual(response.content.decode(), expected_html)
